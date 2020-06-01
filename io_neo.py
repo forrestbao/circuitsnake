72ad69861200f9bd80b6ccea7c8531e8208042a7
forrest <forrest.bao@gmail.com>
2020-06-01 10:45:52 -0500

    add joystick analog input

--- io_neo.py
+++ io_neo.py
@@ -12,7 +12,7 @@ num_pixels = 5*8
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, auto_write=False)
 
 def dotturn(x, y, color):
-    pos = 0     
+    pos = 0
     buttons = {}
     button_keys = {"up":2, "down":4,"left":5, "right":3}
     for direction, pin in button_keys.items():
@@ -20,12 +20,12 @@ def dotturn(x, y, color):
         #print (x)
         buttons[direction] = digitalio.DigitalInOut(xx)
         buttons[direction].direction = digitalio.Direction.INPUT
-        
-    while True: 
-        pixels.fill((0,0,0))  # clean the screen 
-        pixels.show() 
-        
-        # now read button input 
+
+    while True:
+        pixels.fill((0,0,0))  # clean the screen
+        pixels.show()
+
+        # now read button input
         if buttons["left"].value == 0:
             print ("left")
             y = (y - 1 +8)%8
@@ -38,58 +38,54 @@ def dotturn(x, y, color):
         elif buttons["down"].value == 0:
             print ("down")
             x = (x+1)%5;
-        
-        # set cursor 
+
+        # set cursor
         led = matrix2array(x,y)
-        pixels[led] = wheel(pos) 
+        pixels[led] = wheel(pos)
         pixels.show()
-            
+
         time.sleep(0.15)
-        pos += 2 # change the color 
+        pos += 2 # change the color
 
 def dotturn_joystick(x, y, color):
-    pos = 0 
-    
+    pos = 0
+
     joy_h, joy_v = analogio.AnalogIn(board.A0), \
                analogio.AnalogIn(board.A1)
-               
-    value_h, value_v = joy_h.value, joy_v.value 
-        
-    while True: 
-        pixels.fill((0,0,0))  # clean the screen 
-        pixels.show() 
-        
-        # now read button input 
-        new_value_h, new_value_v = joy_h.value, joy_v.value
-        delta_h, delta_v = new_value_h - value_h, new_value_v - value_v 
-        
-        
-        # determining dominant direction 
-        if math.fabs(math.fabs(delta_h) - math.fabs(delta_v)) > 30 and \
-           math.fabs(delta_h) > 30 and   math.fabs(delta_v) > 30 :
-            # denoising
-            if math.fabs(delta_h) > math.fabs(delta_v): 
-                if delta_h > 0 : # left 
-                    print ("left")
-                    y = (y - 1 +8)%8
-                else : # right 
-                    print ("right")
-                    y = (y - 1 +8)%8
-            else:
-                if delta_v > 0 : # up 
-                    print ("up")
-                    x = (x-1+5)%5
-                else: # down 
-                    print ("down")
-                    x = (x+1)%5;
-        
-        # set cursor 
-        led = matrix2array(x,y)
-        pixels[led] = wheel(pos) 
+
+    value_h, value_v = joy_h.value, joy_v.value
+
+    while True:
+        pixels.fill((0,0,0))  # clean the screen
         pixels.show()
-            
+        # right and up  result in  65536
+
+        value_h, value_v = joy_h.value, joy_v.value
+
+        print (value_h, value_v, math.fabs(math.fabs(value_h) -(math.fabs(value_v))))
+
+        if math.fabs(math.fabs(value_h) -(math.fabs(value_v))) > 20000 : # one direciton is dominant
+            if value_h > 50000 :  # right
+                print ("right")
+                y = (y +1)%8
+            elif value_h < 5000: # left
+                y = (y -1 +8)%8
+                print ("left")
+            elif value_v > 50000: # up
+                print ("up")
+                x = (x-1+5)%5
+            elif value_v < 5000: # down
+                print ("down")
+                x = (x+1)%5
+
+
+        # set cursor
+        led = matrix2array(x,y)
+        pixels[led] = wheel(pos)
+        pixels.show()
+
         time.sleep(0.2)
-        pos += 2 # change the color  
+        pos += 2 # change the color
 
 RED = (255, 0, 0)
 YELLOW = (255, 150, 0)
